077880f385c673bf0b37318686f3cae9b0352b7c
Evanlol123 <116678253+Evanlol123@users.noreply.github.com>
2024-05-21 07:28:23 -0400

    :bug:

--- wax/sh1mmer_legacy/root/noarch/usr/sbin/sh1mmer_payload.sh
+++ wax/sh1mmer_legacy/root/noarch/usr/sbin/sh1mmer_payload.sh
@@ -61,7 +61,7 @@ mapname() {
 		'reset-kern-rollback.sh') echo -n "Reset kernel rollback version" ;;
 		'wp-disable.sh') echo -n "WP disable loop (for pencil method)" ;;
   		'kvs.sh') echo -n "Switch your kernel version (kvs)" ;;
-    		'gbbflags.sh') echo -n "Edit your GBB flags ;; 
+    		'gbbflags.sh') echo -n "Edit your GBB flags" ;; 
       		'wifi.sh') echo -n "Connect to wifi" ;;
       		'weston.sh') echo -n "Launch the weston Desktop Environment. REQUIRES A DEVSHIM" ;;
 		*) echo -n "$1" ;;
