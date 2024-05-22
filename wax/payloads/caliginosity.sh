2d1e6b94535b54e7318d83fba6251381a2326c3e
Evanlol123 <116678253+Evanlol123@users.noreply.github.com>
2024-05-22 00:27:03 -0400

    Update caliginosity.sh

--- wax/payloads/caliginosity.sh
+++ wax/payloads/caliginosity.sh
@@ -2,7 +2,7 @@
 
 echo "CALIGINOSITY :: SH1mmer payload for re-enrolling"
 echo ""
-echo "THIS WILL RE-ENROLL YOUR CHROMEBOOK, ATTEMPT TO FIX GBB FLAGS, DISABLE USB BOOT, BLOCK DEVMODE, AND SET YOUR KERNVER TO 3."
+echo "THIS WILL RE-ENROLL YOUR CHROMEBOOK, ATTEMPT TO FIX GBB FLAGS, DISABLE USB BOOT, BLOCK DEVMODE, SET YOUR KERNVER TO 3, AND ENABLE WP."
 echo "THIS SCRIPT ASSUMES YOU ARE USING STOCK FIRMWARE, AND HAVE WRITE-PROTECT OFF"
 echo ""
 echo "ARE YOU SURE YOU WANT TO DO THIS? [y/N]"
@@ -17,6 +17,9 @@ if [ "$input" = "y" ]; then
 	tpm_manager_client take_ownership
 	cryptohome --action=set_firmware_management_parameters --flags=0x01
 	/usr/share/vboot/bin/set_gbb_flags.sh 0x0
+ 	initctl stop trunksd
+        tpmc write 0x1008 02  4c 57 52 47  3 0 1 0  0 0 0  EC
+	flashrom --wp-enable
 	echo "rebooting"
 	reboot
 	exit
