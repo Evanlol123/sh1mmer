bbb7c629f084ab4583f9fc1339b5e489d4d6265c
Evanlol123 <116678253+Evanlol123@users.noreply.github.com>
2024-05-23 14:03:35 -0400

    Update sh1mmer_main.sh

--- wax/sh1mmer_bw/root/noarch/usr/sbin/sh1mmer_main.sh
+++ wax/sh1mmer_bw/root/noarch/usr/sbin/sh1mmer_main.sh
@@ -10,9 +10,10 @@ showbg Disclaimer.png
 sleep 1
 read -rsn1
 
-mkdir -p /mnt/sh1mmer /usr/local
+mkdir -p /mnt/sh1mmer /usr/local /recovery_images
 if mount /dev/disk/by-label/SH1MMER /mnt/sh1mmer >/dev/null 2>&1; then
-	mount --bind /mnt/sh1mmer/chromebrew /usr/local >/dev/null 2>&1 || umount /mnt/sh1mmer
+	mount --bind /mnt/sh1mmer/chromebrew /usr/local >/dev/null 2>&1
+	mount --bind /mnt/sh1mmer/recovery_images /recovery_images >/dev/null 2>&1
 fi
 
 loadmenu() {
